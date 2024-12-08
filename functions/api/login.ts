a27ad18b4192aab49224c1e486204b97ca37a960
Hein Pieterse <22915998+hpieterse@users.noreply.github.com>
2024-12-08 06:42:44 +0100

    feat: add ZT API integration

--- functions/api/login.ts
+++ functions/api/login.ts
@@ -1,11 +1,21 @@
+import createClient from 'openapi-fetch'
 import { Env } from '../environment'
+import type { paths } from '../models/zerotier/schema'
 
-export const onRequestPost: PagesFunction<Env> = async ({ request }) => {
+export const onRequestPost: PagesFunction<Env> = async ({ env, request }) => {
   const { token } = (await request.json()) as { token?: string }
 
-  if (token == null || token == 'invalid') {
-    return new Response('Unauthorized', { status: 401 })
+  const client = createClient<paths>({ baseUrl: env.ZT_API_URL })
+
+  const { data } = await client.GET('/status', {
+    headers: {
+      Authorization: `token ${token}`,
+    },
+  })
+
+  if (data.user != null) {
+    return new Response('OK')
   }
 
-  return new Response('OK')
+  return new Response('Unauthorized', { status: 401 })
 }
--- functions/environment.ts
+++ functions/environment.ts
@@ -1,3 +1,4 @@
 export type Env = {
   ACCESS_CONTROL_ALLOW_ORIGIN?: string
+  ZT_API_URL: string
 }
