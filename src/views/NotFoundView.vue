cc8e29a0c10ee4d2405be1de37fa9769140a5e7c
Hein Pieterse <22915998+hpieterse@users.noreply.github.com>
2024-12-08 20:59:43 +0100

    feat: add button to 404 page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+import StyledLink from '@/components/StyledLink.vue'
+</script>
+
 <template>
-  <h1 class="text-center text-6xl font-bold mt-16">404</h1>
+  <div class="flex flex-col justify-center items-center">
+    <h1 class="text-center text-6xl font-bold mt-16">404</h1>
+    <StyledLink to="/" class="block mt-8 text-center"> Go back home </StyledLink>
+  </div>
 </template>
